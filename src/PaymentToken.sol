1ea3606d7c741eaadea11638702bc90fd84e70f8
GarethSigP <gareth@sigmaprime.com>
2026-09-01 20:44:56 +0200

    Deleted Collector to get back to bare bones

--- src/PaymentToken.sol
+++ src/PaymentToken.sol
@@ -3,18 +3,13 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract PaymentToken is ERC20 {
-
-    address public coinOwner;
+contract HumewoodZAR is ERC20 {
     
-    constructor(address _coinOwner) ERC20("PaymentToken", "SAPT") {
-        require(_coinOwner != address(0), "PaymentToken: _coinOwner Address Zero");
-        coinOwner = _coinOwner;
+    constructor() ERC20("Humewood ZAR", "HMZ") {
+      
     }
 
     function mint(address _to, uint256 _amount) external {
-        require(_to != address(0), "PaymentToken: _to Address Zero");
-        require(msg.sender == coinOwner, "PaymentToken: Not Permissioned To Mint");
-        _mint(_to, _amount);
+      
     }
 }
